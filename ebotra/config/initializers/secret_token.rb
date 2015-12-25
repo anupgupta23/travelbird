8f8f5b29005c51366138d43c2d05a5f7dc7daf88
anhad <anhad.mohananey@infibeam.net>
2015-12-25 18:35:01 +0530

    Bare rails app

---
+++ ebotra/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebotra::Application.config.secret_token = '6cec96ac518e199e95ecc64e6f6009c3056f127071ecb695a8cdd24b3493677656d8c539a946d43a19cb8571ebd89567344bb7410c64d6043e9a0a2e2187ebf2'
