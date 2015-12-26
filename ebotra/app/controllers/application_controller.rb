01303c09300fafe2dead35e43b35d50c96647fdc
anhad <anhad.mohananey@infibeam.net>
2015-12-26 17:14:54 +0530

    GemFile fixed + wrap in transaction

--- ebotra/app/controllers/application_controller.rb
+++ ebotra/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  around_filter :wrap_in_transaction
+
+  def wrap_in_transaction
+    begin
+      ActiveRecord::Base.transaction do
+        yield
+      end
+    end
+  end
+
 end
