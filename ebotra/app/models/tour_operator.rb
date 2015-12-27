501b29cff2c67867ee9c255fbb92f784ef442dd9
anhad <anhad.mohananey@infibeam.net>
2015-12-27 22:55:02 +0530

    fixes

--- ebotra/app/models/tour_operator.rb
+++ ebotra/app/models/tour_operator.rb
@@ -1,5 +1,5 @@
 class TourOperator < ActiveRecord::Base
-  has_and_belongs_to_many :tour, :through=>"tours_operators_map"
+  has_and_belongs_to_many :tour, :join_table=>"tours_operators_map"
 
 
 end
