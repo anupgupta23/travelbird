3afa20de791d4f6e5f87fc0749ae6bda67b8b530
Tushar Agarwal <tusharag171@gmail.com>
2016-01-09 10:45:17 +0530

    updating city

--- ebotra/db/migrate/20151227153151_create_table_city.rb
+++ ebotra/db/migrate/20151227153151_create_table_city.rb
@@ -3,7 +3,9 @@ class CreateTableCity < ActiveRecord::Migration
     create_table :cities do |t|
       t.integer :country_id
       t.string :name
-      t.text :places_and_attractions
+      t.text :weather
+      t.text :best_time_of_visit
+      t.text :other_info
     end
   end
 
